ecad1d2ad89d46416f41f694f745af097a1e9ec1
Michael Di Cesare <mvdicesare@gmail.com>
2019-09-25 14:27:09 -0600

    master

---
+++ Task/Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task
+//
+//  Created by Michael Di Cesare on 9/25/19.
+//  Copyright © 2019 Michael Di Cesare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
